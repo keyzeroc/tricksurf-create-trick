18b0195e0e588f3955e36996e220b8c47a821f3d
keyzeroc <gozveruga@gmail.com>
2024-07-01 20:34:41 +0300

    fix2

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,7 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export function getImageURL(name: string) {
-  return new URL(`../assets/triggers/${name}.jpg`, import.meta.url).href;
-}
